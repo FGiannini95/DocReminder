067569ececfdb41658c9d86a77a33b5b1521158b
federico <fgiannini95@gmail.com>
2026-03-03 22:50:43 +0000

    Clean up unused routes

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 
 var app = express();
@@ -17,8 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // API routes
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
