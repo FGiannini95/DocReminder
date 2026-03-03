6f523f831edb9a1e184da0724e814869a729f0b1
federico <fgiannini95@gmail.com>
2026-03-03 23:29:36 +0000

    First implementation sendOtp

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,13 +1,41 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const connection = require("../config/db");
+const db = require("../config/db");
 
 class authController {
-  test1 = (req, res) => {
-    console.log("Hi from test1");
+  sendOtp = async (req, res) => {
+    const { email } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+    // Email is invalid
+    if (!email || !emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid request" });
+    }
+
+    try {
+      // Select user
+      let selectUser = `SELECT user_id, email FROM user WHERE email = ? AND is_deleted = 0`;
+
+      const [rows] = await db.query(selectUser, [email]);
+      const user = rows[0];
+
+      if (!user) {
+        // User doesn't exist, create it
+        const insertUser = `INSERT INTO user (email) VALUES (?)`;
+        await db.query(insertUser, [email]);
+      }
+      // Generate OTP
+      // Save OTP to DB
+      // Send email
+      // Respond
+    } catch (err) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
   };
-  test2 = (req, res) => {
-    console.log("Hi from test2");
+
+  verifyOtp = (req, res) => {
+    console.log("body:", req.body);
+    res.json({ message: "ok" });
   };
 }
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const authController = require("../controllers/authController");
 
 // base path http://localhost:3000/auth
-router.post("/otp", authController.test1);
-router.post("/otp/verify", authController.test2);
+router.post("/otp", authController.sendOtp);
+router.post("/otp/verify", authController.verifyOtp);
 
 module.exports = router;
