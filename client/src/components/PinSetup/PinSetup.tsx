6c2cdd3e9e87653b738a6fc44090debf7870c0a8
federico <fgiannini95@gmail.com>
2026-03-05 23:29:27 +0000

    Create new future components

---
+++ client/src/components/PinSetup/PinSetup.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PinSetup = () => {
+  return <div>PinSetup</div>;
+};
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,3 @@
 export { PageTransition } from "./PageTransition/PageTransition";
+export { PinSetup } from "./PinSetup/PinSetup";
+export { BiometricSetup } from "./BiometricSetup/BiometricSetup.tsx";
