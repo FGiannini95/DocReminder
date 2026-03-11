a6cb60865860c8b9599c5ba3b80f869be2553ed2
federico <fgiannini95@gmail.com>
2026-03-11 23:06:15 +0000

    improving Ui

--- client/src/components/PageTransition/PageTransition.tsx
+++ client/src/components/PageTransition/PageTransition.tsx
@@ -6,7 +6,7 @@ export const PageTransition = ({ children }: { children: React.ReactNode }) => {
     <motion.div
       initial={{ opacity: 0, x: 40 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -40 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.2, ease: "easeInOut" }}
       style={{ height: "100%" }}
     >
