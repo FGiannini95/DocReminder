cd318a1f1640c63033d50858e60e40a1780a4dbe
federico <fgiannini95@gmail.com>
2026-03-17 12:22:07 +0000

    Improving editDocument controller

--- server/controllers/docController.js
+++ server/controllers/docController.js
@@ -34,11 +34,16 @@ class docController {
   };
 
   editDocument = async (req, res) => {
-    console.log("Hi froma editaxiosInDoc");
     const { id: documentId } = req.params;
     const userId = req.user.userId;
-    const { type, document_number, expiry_date, reminder_days, personal_note } =
-      req.body;
+    const {
+      type,
+      name,
+      document_number,
+      expiry_date,
+      reminder_days,
+      personal_note,
+    } = req.body;
 
     if (!type || !expiry_date) {
       return res.status(400).json({ message: "Missing required fields" });
@@ -51,15 +56,16 @@ class docController {
     try {
       const editDocument = `
         UPDATE document SET
-          type = ?, document_number = ?, expiry_date = ?, reminder_days = ?, personal_note = ?
+          type = ?, name = ?, document_number = ?, expiry_date = ?, reminder_days = ?, personal_note = ?
         WHERE user_id = ? AND document_id = ?
       `;
 
       await db.query(editDocument, [
         type,
+        name,
         document_number,
         expiry_date,
-        reminder_days,
+        JSON.stringify(reminder_days),
         personal_note,
         userId,
         documentId,
@@ -67,6 +73,8 @@ class docController {
 
       return res.status(200).json({ message: "Document updated succesfully" });
     } catch (err) {
+      console.log(err);
+
       res.status(500).json({ message: "Internal server error" });
     }
   };
