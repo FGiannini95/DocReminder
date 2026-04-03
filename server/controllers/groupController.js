3134c84a3ef0ead10461e314709519e8d202bb00
federico <fgiannini95@gmail.com>
2026-04-03 22:35:04 +0100

    Skeeleton controller and routes

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -3,7 +3,30 @@ const jwt = require("jsonwebtoken");
 
 class groupController {
   addGroup = async (req, res) => {
-    console.log("Hi from addGroup");
+    // extract name and icon from request body
+    // extract userId from req.user
+
+    // if name is missing, return 400
+
+    try {
+      // insert new group into private_groups with name, icon, admin_id
+      // return 201 with the new group id and name
+    } catch (err) {
+      // return 500
+    }
+  };
+
+  getOneGroup = async (req, res) => {
+    console.log("Hi from getOneGroup");
+  };
+  getAllGroup = async (req, res) => {
+    console.log("Hi from getAllGroup");
+  };
+  editGroup = async (req, res) => {
+    console.log("Hi from editGroup");
+  };
+  deleteGroup = async (req, res) => {
+    console.log("Hi from deleteGroup");
   };
 }
 
--- server/routes/group.js
+++ server/routes/group.js
@@ -4,5 +4,9 @@ const groupController = require("../controllers/groupController");
 
 // base path http://localhost:3000/groups
 router.post("/add-group", groupController.addGroup);
+router.get("/:id", groupController.getOneGroup);
+router.get("/", groupController.getAllGroup);
+router.put("/edit-group/:id", groupController.editGroup);
+router.delete("/delete-group/:id", groupController.deleteGroup);
 
 module.exports = router;
