92262c94281dd0a896a977b3caa19198577a7428
federico <fgiannini95@gmail.com>
2026-04-04 23:02:52 +0100

    Implement add group logic

--- client/src/pages/Group/components/GroupCard/GroupCard.tsx
+++ client/src/pages/Group/components/GroupCard/GroupCard.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Box, Button, Typography } from "@mui/material";
 
 import { EmptyState } from "../../../../components/EmptyState/EmptyState";
+import { GroupDrawer } from "../GroupDrawer/GroupDrawer";
 
 export const GroupCard = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <Box sx={{ px: 3 }}>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-          <Typography variant="h6" fontWeight="bold">
-            Mis grupos:
-          </Typography>
-          <Button variant="contained" size="small" color="inherit">
-            + Crear grupo
-          </Button>
+    <>
+      <Box sx={{ px: 3 }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+            <Typography variant="h6" fontWeight="bold">
+              Mis grupos:
+            </Typography>
+            <Button variant="contained" size="small" color="inherit" onClick={() => setOpen(true)}>
+              + Crear grupo
+            </Button>
+          </Box>
+          <EmptyState message="Aún no has creado ningún grupo." />
         </Box>
-        <EmptyState message="Aún no has creado ningún grupo." />
       </Box>
-    </Box>
+      <GroupDrawer open={open} onClose={() => setOpen(false)} />
+    </>
   );
 };
