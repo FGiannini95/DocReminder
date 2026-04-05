0231c32a900bfdff4d7e1233d796bcd54f7cf761
federico <fgiannini95@gmail.com>
2026-04-05 21:59:52 +0100

    Adding GroupHeader

--- client/src/pages/OneGroup/OneGroup.tsx
+++ client/src/pages/OneGroup/OneGroup.tsx
@@ -3,8 +3,9 @@ import { useNavigate } from "react-router-dom";
 
 import { Box } from "@mui/material";
 
-import { DocumentHeader, PageTransition } from "@/components";
+import { PageTransition } from "@/components";
 import { DocReminderRoutes } from "@/routes/routes";
+import { GroupHeader } from "../OneDocument/components/GroupHeader/GroupHeader";
 
 export const OneGroup = () => {
   const navigate = useNavigate();
@@ -12,10 +13,7 @@ export const OneGroup = () => {
     <PageTransition>
       <Box sx={{ minHeight: "100dvh", display: "flex", flexDirection: "column" }}>
         {/* Header */}
-        <DocumentHeader
-          title="Un grupo y ya está"
-          onBack={() => navigate(DocReminderRoutes.home)}
-        />
+        <GroupHeader title="Un grupo y ya está" onBack={() => navigate(DocReminderRoutes.home)} />
       </Box>
     </PageTransition>
   );
