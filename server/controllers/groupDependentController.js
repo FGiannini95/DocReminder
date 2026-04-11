4caf33a599d91c26372f5cf2234ec4bc98cf336c
federico <fgiannini95@gmail.com>
2026-04-11 23:17:42 +0100

    Skeleton of the addDependant controller

--- server/controllers/groupDependentController.js
+++ server/controllers/groupDependentController.js
@@ -3,7 +3,17 @@ const jwt = require("jsonwebtoken");
 
 class groupDependentController {
   addDependent = async (req, res) => {
-    console.log("Hi from addDependent");
+    // extract groupId from req.params
+    // extract name, relationship, birth_date, avatar from req.body
+
+    // if name missing return 400
+
+    try {
+      // INSERT INTO group_dependents (group_id, name, relationship, birth_date, avatar)
+      // return 201 with new dependent id
+    } catch (err) {
+      // return 500
+    }
   };
 }
 
