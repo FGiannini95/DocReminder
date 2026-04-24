a3d0e97337cbcf62163724dc5ddf5a0e8de4acf0
federico <fgiannini95@gmail.com>
2026-04-24 14:23:06 +0100

    Remove unsed React declaration

--- client/src/pages/Group/components/GroupCard/GroupCard.tsx
+++ client/src/pages/Group/components/GroupCard/GroupCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Box, Button, Card, CardContent, Typography } from "@mui/material";
