0a24ef3b7664f6813d97132069837b691172542f
federico <fgiannini95@gmail.com>
2026-04-29 10:28:02 +0100

    Revert changes

--- server/utils/cronScheduler.js
+++ server/utils/cronScheduler.js
@@ -2,6 +2,6 @@ const cron = require("node-cron");
 const sendDailyReminders = require("./reminderService");
 
 // Run every day at 9.00
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 9 * * *", async () => {
   await sendDailyReminders();
 });
